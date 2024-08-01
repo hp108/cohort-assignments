2e7663640d7659743ef807944d8953857c158cf5
hp108 <98585554+hp108@users.noreply.github.com>
2024-08-01 17:53:16 +0000

    initial commit

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -11,11 +11,13 @@
      Example: GET http://localhost:3000/file/example.txt
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
- */
+ */ 
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+
+
 
 
 module.exports = app;
